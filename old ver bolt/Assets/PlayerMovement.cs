04092fd5836d0919ff817ab53b648b493b2128ad
Caroline Ling <caroline@Carolinede-MacBook-Pro.local>
2021-02-27 17:49:56 +0800

    movement working

---
+++ old ver bolt/Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿
+
+using UnityEngine;
+
+public class PlayerMovement : Bolt.EntityBehaviour<ICustomCubeState>
+{
+	public override void Attached(){
+		state.SetTransforms(state.CubeTransform, transform);
+	}
+
+	public override void SimulateOwner(){
+		var speed = 4f;
+		var movement = Vector3.zero;
+
+		if(Input.GetKey(KeyCode.A)){
+			movement.x -= 1f;
+		}
+
+		if(Input.GetKey(KeyCode.D)){
+			movement.x += 1f;
+		}
+
+		if(Input.GetKey(KeyCode.W)){
+			movement.z += 1f;
+		}
+
+		if(Input.GetKey(KeyCode.S)){
+			movement.z -= 1f;
+		}
+
+		if(movement != Vector3.zero){
+			transform.position = transform.position + (movement.normalized * speed * BoltNetwork.FrameDeltaTime);
+		}
+
+	}
+
+
+}
